3a4dc67a82ffbc5f51993b9beb1f407699666d95
mohamed-rahmani <mohamed.rahmani@edu.univ-paris13.fr>
2024-12-18 22:26:38 +0100

    added: init desktop app project with electron + react + vite

---
+++ apps/desktop/vite.config.ts
@@ -0,0 +1,15 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: "./",
+  build: {
+    outDir: "dist-react",
+  },
+  server: {
+    port: 5123,
+    strictPort: true,
+  },
+});
