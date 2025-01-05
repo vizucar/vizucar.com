952c678cd3f007711d7d0d8182e5817f1860a155
Yannick <yk.105m@gmail.com>
2025-01-05 01:30:39 +0100

    add visual of vizucar(test-version) and how-it-works's section

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -5,6 +5,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Image from "next/image";
+import Link from "next/link";
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -23,7 +24,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className={clsx(inter.className, "bg-background")}>
       <section className="relative h-screen">
-          <div className="absolute top-0 right-0 w-8/10 h-full bg-[url('/audi-r8-rouge.jpg')] bg-cover bg-no-repeat bg-[length:130%]"></div>
+          <div className="absolute top-0 right-0 w-8/10 h-full bg-[url('/audi-r8-rouge.jpg')] bg-cover bg-no-repeat bg-[length:135%]"></div>
 
           <div className="relative z-10 ">
             <Header />
@@ -72,9 +73,30 @@ export default function RootLayout({
 
 
 
-        <div className="bg-gradient-to-b from-black via-[#161414] to-black">
-          <section className="text-white p-4">Section</section>
-        </div>
+          <section className="bg-gradient-to-b from-black to-[#161414]  text-center text-white p-4 h-full flex flex-col ">
+            <h1 id="how-it-works" className="text-7xl font-bold mt-[100px]">How does it Works</h1>
+              <p className="text-gray-100 text-4xl mt-[100px]">
+                Vizucar allows his user to search a car among thousands of vehicles.
+                All of this is possible due to the Vizucar API
+              </p>
+          </section>
+          <section className="bg-[#161414] p-4 h-full flex items-center justify-center">
+            <Image className="rounded mt-[100px] mb-[100px]"
+              src={"/vizucar-visual.png"}
+              alt="window's logo by Smashicons"
+              width={1000}
+              height={1020}
+            />
+          </section>
+          <section className="bg-gradient-to-b from-[#161414] to-black text-cente p-4 h-full flex flex-col items-center justify-center">
+            <h1 id="team" className="text-7xl font-bold">Team</h1>
+          </section>
+          <article>
+            <Link href={"https://github.com/oshio10"}></Link>
+            <Link href={"https://github.com/mohamed-rahmani"}></Link>
+            <Link href={"https://github.com/K-Yann-K"}></Link>
+            <Link href={"https://github.com/vithurzen"}></Link>
+          </article>
         <Footer />
       </body>
     </html>
